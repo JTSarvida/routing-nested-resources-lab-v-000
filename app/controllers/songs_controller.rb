ebac4e5466c9a2f33771d1bafa707700252bd6a8
Joshua Tan <jtsarvida@gmail.com>
2020-05-20 21:00:36 +0000

    Automatically backed up by Learn

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -1,6 +1,7 @@
 class SongsController < ApplicationController
   def index
     if params[:artist_id]
+      @artists = Artist.all
       @artist = Artist.find_by(id: params[:artist_id])
       if @artist.nil?
         redirect_to artists_path, alert: 'Artist Not found'
