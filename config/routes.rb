d62fb41782215b753068dbddaf93dffef7a9246e
Joshua Tan <jtsarvida@gmail.com>
2020-05-20 21:10:12 +0000

    Automatically backed up by Learn

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
     resources :songs, only: [:show, :index]
   end
 
-  resources :songs
+  resources :songs, only: [:index, :show, :new, :create, :edit, :update, :destroy]
 
   root 'songs#index'
 end
